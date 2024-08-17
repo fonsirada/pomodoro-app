80b7babc8a151bbb49bbbd57e801e0f444697a11
fonsirada <fonsirada13@gmail.com>
2024-08-16 22:18:12 -0400

    got rid of background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-//makes chrome send over a notification
-const sendNotification = () => {
-    chrome.notifications.create({
-        type: 'basic',
-        iconUrl: 'icons/pomodoro.png',
-        title: 'Pomodoro Timer Extension',
-        message: 'Time is up! Open the extension to start the next period.'
-    });
-};
